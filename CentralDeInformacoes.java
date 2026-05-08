37bd00d6037e5b8267929fc8bd1f2304a47f7662
Nycholas Richard Cosmo Silva <121464739+nycholasrd@users.noreply.github.com>
2026-05-08 12:18:26 -0300

    Add files via upload

---
+++ CentralDeInformacoes.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class CentralDeInformacoes {
+	private ArrayList<Pessoa> todasAsPessoas = new ArrayList<>();
+	
+	public boolean adicionarPessoas(Pessoa pessoa) {
+		for(Pessoa testeP : todasAsPessoas) {
+			if(testeP.getCpf().equals(pessoa.getCpf())){
+				return false;
+			}
+		}
+		todasAsPessoas.add(pessoa);
+		return true;
+		
+	}
+
+	public ArrayList<Pessoa> getTodasAsPessoas() {
+		return todasAsPessoas;
+	}
+
+	public void setTodasAsPessoas(ArrayList<Pessoa> todasAsPessoas) {
+		this.todasAsPessoas = todasAsPessoas;
+	}
+	
+	public Pessoa recuperarPessoaPorCPF(String cpf) {
+		for(Pessoa pessoasL : todasAsPessoas) {
+			if(pessoasL.getCpf().equals(cpf)) {
+				return pessoasL;
+			}
+		}
+			return null;
+	}
+}
