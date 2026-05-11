6a8f9d0eb573066cdf02b42e4a0ab8798609aae0
Nycholas Richard Cosmo Silva <121464739+nycholasrd@users.noreply.github.com>
2026-05-11 09:18:40 -0300

    Adicionando o método recuperarPropostaPorId que não foi adicionado na ultima atualização

--- CentralDeInformacoes.java
+++ CentralDeInformacoes.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 
 public class CentralDeInformacoes {
 	private ArrayList<Pessoa> todasAsPessoas = new ArrayList<>();
+	private ArrayList<PropostaDeAluguel> todasAsPropostas = new ArrayList<>();
+	
 	
 	public boolean adicionarPessoas(Pessoa pessoa) {
 		for(Pessoa testeP : todasAsPessoas) {
@@ -22,6 +24,39 @@ public class CentralDeInformacoes {
 		this.todasAsPessoas = todasAsPessoas;
 	}
 	
+	public boolean adicionarPropostas(PropostaDeAluguel p) {
+		for(PropostaDeAluguel testePp : todasAsPropostas) {
+			if(testePp.getId() == p.getId()) {
+				return false;
+			}
+		}
+		todasAsPropostas.add(p);
+		return true;
+	}
+	
+	public PropostaDeAluguel recuperarPropostaPorld(long id) {
+		for(PropostaDeAluguel testePp : todasAsPropostas) {
+			if(testePp.getId() == id) {
+				return testePp;
+			}
+		}
+		return null;
+	}
+	
+	public ArrayList<PropostaDeAluguel> recuperarPropostaDeUmaPessoa(String cpf) {
+		ArrayList<PropostaDeAluguel> propostasPorLocatario = new ArrayList<>();
+		
+		
+		for(PropostaDeAluguel procura : todasAsPropostas) {
+			if(procura.getLocatario() == null) {
+				return null;
+			}
+			if(procura.getLocatario().equals(cpf)) {
+				propostasPorLocatario.add(procura);
+			}
+		}
+		return propostasPorLocatario;
+	}
 	public Pessoa recuperarPessoaPorCPF(String cpf) {
 		for(Pessoa pessoasL : todasAsPessoas) {
 			if(pessoasL.getCpf().equals(cpf)) {
